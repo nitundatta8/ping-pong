b2d817d6432b7545ac02347c0c6f00456a14b7a4
mai cook <mailiecook@yahoo.com>
2020-05-11 14:04:39 -0700

    add 4 tests, all testing passing

---
+++ pingpong.solution/pingpongstuff/Models/main.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Ping
+{
+  public class InputOutput
+  {
+    static void Main()
+    {
+      //takes user's input; input question
+      Console.WriteLine("Enter a number");
+      string str = Console.ReadLine();
+      int number = int.Parse(str);
+      //stores the input and makes it an integer; output
+
+
+      
+    }
+  }
+}
